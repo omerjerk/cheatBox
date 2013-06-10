b1a3a43e8f923871d36e78cfeab822830ed28f72
Umair Khan <omerjerk@gmail.com>
2013-06-10 22:55:45 +0530

    fixes

--- src/com/omerjerk/cheatbox/MainActivity.java
+++ src/com/omerjerk/cheatbox/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 import com.omerjerk.cheatbox.fragments.mainFragment;
@@ -52,6 +54,22 @@ public class MainActivity extends SlidingFragmentActivity {
 				setSlidingActionBarEnabled(false);
 				getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			toggle();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 	
 	public void switchContent(Fragment fragment) {
